4fc2e1fba9bf4f06891f942c5a693e3ee4271046
Lazy <1282394741@qq.com>
2021-06-29 16:48:18 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
+  </div>
+</template>
+
+<script>
+import mainTabBar from "./assets/components/content/mainTabBar/mainTabBar.vue";
+export default {
+  components: {
+    mainTabBar,
+  },
+};
+</script>
+
+<style>
+</style>
