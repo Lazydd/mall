60dd3bbce46d81afea98e5deeffee3b9b2507951
Lazy <1282394741@qq.com>
2021-06-29 17:20:16 +0800

    初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/home/Home.vue'
-import Lei from '../views/lei/Lei.vue'
-import Shop from '../views/shop/Shop.vue'
-import Me from '../views/me/Me.vue'
+import Home from '@/views/home/Home.vue'
+import Lei from '@/views/lei/Lei.vue'
+import Shop from '@/views/shop/Shop.vue'
+import Me from '@/views/me/Me.vue'
 
 const routes = [
   {
